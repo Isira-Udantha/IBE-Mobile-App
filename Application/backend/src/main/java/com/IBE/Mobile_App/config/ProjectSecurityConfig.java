d2c685d965d43cbf6eaeefe479c5dde45b12cda1
Isira Udantha <e19253@eng.pdn.ac.lk>
2024-08-23 12:21:28 +0530

    Add flightdesignator to Isira branch

---
+++ Application/backend/src/main/java/com/IBE/Mobile_App/config/ProjectSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.IBE.Mobile_App.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers().authenticated()
+                .antMatchers("/api/v1/flight/save","/api/v1/user/register","/api/v1/user/search-user","/api/v1/flight/search-flight-by-date-arrival-and-departure").permitAll()
+                .and().formLogin().and().httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- Application/backend/src/main/java/com/IBE/Mobile_App/controller/UserController.java
+++ Application/backend/src/main/java/com/IBE/Mobile_App/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.IBE.Mobile_App.controller;
 
 import com.IBE.Mobile_App.dto.UserDTO;
+import com.IBE.Mobile_App.entity.User;
+import com.IBE.Mobile_App.repo.UserRepo;
 import com.IBE.Mobile_App.service.UserService;
 import com.IBE.Mobile_App.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,15 +18,43 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepo userRepo;
+
+//    @PostMapping(value = "/register")
+//    public ResponseEntity<StandardResponse> registerUser(@RequestBody UserDTO userDTO){
+//        String id = userService.addUser(userDTO);
+//        return new ResponseEntity<StandardResponse>(
+//                new StandardResponse(201,"user added successfully",id),
+//                HttpStatus.CREATED
+//        );
+//    }
+
     @PostMapping(value = "/register")
-    public ResponseEntity<StandardResponse> saveUser(@RequestBody UserDTO userDTO){
-//        System.out.println(userDTO);
-        String id = userService.addUser(userDTO);
-        return new ResponseEntity<StandardResponse>(
-                new StandardResponse(201,"user added successfully",id),
+    public ResponseEntity<StandardResponse> registerUser(@RequestBody User user){
+        ResponseEntity<StandardResponse> response = null;
+        try {
+            String hashPassword = passwordEncoder.encode(user.getUserPassword());
+            user.setUserPassword(hashPassword);
+            User savedUser = userRepo.save(user);
+            if (savedUser.getUserId()>0){
+                return new ResponseEntity<StandardResponse>(
+                new StandardResponse(201,"user added successfully",savedUser.getUserName()),
+                HttpStatus.CREATED);
+            }
+        }catch (Exception ex){
+                return new ResponseEntity<StandardResponse>(
+                new StandardResponse(201,"Exception Occured",ex.getMessage()),
                 HttpStatus.CREATED
         );
+        }
+        return response;
     }
+
+
 
     @GetMapping(
             value = "/search-user",
